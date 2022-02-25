e20218b4027b4039125d4fadcfbabc0479fdcad3
wy <wyforme88@gmail.com>
2022-02-25 16:55:35 +0800

    整理代码

--- app/src/main/java/com/wy/animation/rotate3d/MyAdapter.java
+++ app/src/main/java/com/wy/animation/rotate3d/MyAdapter.java
@@ -6,22 +6,22 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-public class MyAdapter extends FragmentPagerAdapter{
+public class MyAdapter extends FragmentPagerAdapter {
 
-	private List<Fragment> list;
+    private List<Fragment> list;
 
-	public MyAdapter(FragmentManager fm,List<Fragment> list) {
-		super(fm);
-		this.list=list;
-	}
+    public MyAdapter(FragmentManager fm, List<Fragment> list) {
+        super(fm);
+        this.list = list;
+    }
 
-	@Override
-	public Fragment getItem(int arg0) {
-		return list.get(arg0);
-	}
+    @Override
+    public Fragment getItem(int arg0) {
+        return list.get(arg0);
+    }
 
-	@Override
-	public int getCount() {
-		return list.size();
-	}
+    @Override
+    public int getCount() {
+        return list.size();
+    }
 }
--- app/src/main/java/com/wy/animation/rotate3d/MyFragment.java
+++ app/src/main/java/com/wy/animation/rotate3d/MyFragment.java
@@ -12,7 +12,7 @@ public class MyFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.f, null);
+        View view = inflater.inflate(R.layout.layout_image, null);
         return view;
     }
 }
